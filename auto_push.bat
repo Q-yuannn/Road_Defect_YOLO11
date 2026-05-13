4c70ea6a0f88b7c40feaa197474e92868dbbc92f
Wang Qiyuan <wyuan8770@gmail.com>
2026-05-13 21:52:59 +0800

    修改训练参数，优化训练效果

---
+++ auto_push.bat
@@ -0,0 +1,15 @@
+@echo off
+chcp 65001
+echo ======================
+echo  一键上传代码到GitHub
+echo ======================
+:: 激活 Conda 环境，然后执行命令
+call D:\py\anaconda3\Scripts\activate.bat pytorch_env
+
+git add .
+git commit -m "修改训练参数，优化训练效果"
+git push
+
+echo.
+echo 执行完毕！按任意键关闭窗口...
+pause
--- train_baseline.py
+++ train_baseline.py
@@ -9,7 +9,12 @@ if __name__ == '__main__':
         data='road.yaml',          # 指向配置文件
         epochs=100,                # 训练100轮
         imgsz=640,                 # 图像输入尺寸 (论文标准尺寸)
-        batch=16,                  
+        batch=16,          
+        optimizer='SGD',           # 优化器使用 SGD
+        lr0=0.01,                  # 初始学习率 0.01
+        momentum=0.937,            # 动量参数 0.937
+        weight_decay=0.0005,       # 权重衰减 0.0005
+
         device=0,                  # 指定使用第一张显卡 (RTX 4060)
         workers=4,                 # 开启多线程数据加载加速
         name='baseline_yolo11n',   # 训练结果保存的文件夹名称
