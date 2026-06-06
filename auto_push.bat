1ba26844b2b0fcba86c784dd51127495577378da
Wang Qiyuan <wyuan8770@gmail.com>
2026-06-06 13:08:24 +0800

    生成 D00 带框预览图，方便后续筛选RDD中有效图和框数据，新建review_D00_bad文件夹存储不合格的D00图和框数据，后续人工审核并删除不合格的数据。

--- auto_push.bat
+++ auto_push.bat
@@ -7,7 +7,7 @@ echo ======================
 call D:\py\anaconda3\Scripts\activate.bat pytorch_env
 
 git add .
-git commit -m "生成 D00 带框预览图，方便后续筛选RDD中有效图和框数据"
+git commit -m "生成 D00 带框预览图，方便后续筛选RDD中有效图和框数据，新建review_D00_bad文件夹存储不合格的D00图和框数据，后续人工审核并删除不合格的数据。"
 git push
 
 echo.
